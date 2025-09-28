7bac626847e6fbf8857e180f215556e63efc9914
chandu <chandrasekharr.gaddam@gmail.com>
2025-09-28 09:47:27 +0530

    changes

--- main_test.go
+++ main_test.go
@@ -9,3 +9,11 @@ func TestAdd(t *testing.T) {
 		t.Errorf("add(2, 3) = %d; want %d", got, want)
 	}
 }
+
+func TestMultiply(t *testing.T) {
+	got := multiply(2, 3)
+	want := 6
+	if got != want {
+		t.Errorf("multiply(2, 3) = %d; want %d", got, want)
+	}
+}
