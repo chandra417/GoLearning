a954bda13b9b6550eb1c3b936c28a0f3eeba1011
chandu <chandrasekharr.gaddam@gmail.com>
2025-09-28 10:18:51 +0530

    slice if for

--- main.go
+++ main.go
@@ -10,6 +10,14 @@ func multiply(a int, b int) int {
 	return a * b
 }
 
+func oddEven(a int) string {
+	if a%2 == 0 {
+		return fmt.Sprintf("%d is a even number\n", a)
+	} else {
+		return fmt.Sprintf("%d is a odd number\n", a)
+	}
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("This is my first commit")
--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	got := add(2, 3)
@@ -15,5 +18,25 @@ func TestMultiply(t *testing.T) {
 	want := 6
 	if got != want {
 		t.Errorf("multiply(2, 3) = %d; want %d", got, want)
+	}
+}
+
+func TestSumOfNumbers(t *testing.T) {
+	nums := []int{1, 2, 3}
+	sum := 0
+
+	for _, n := range nums {
+		sum += n
+	}
+	if sum != 6 {
+		t.Errorf("sum = %d; want 6", sum)
+	}
+}
+
+func TestOddEvenNumbers(t *testing.T) {
+	
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	for _, n := range nums {
+		fmt.Println(oddEven(n))
 	}
 }
